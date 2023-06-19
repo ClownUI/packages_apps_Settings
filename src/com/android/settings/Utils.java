3a891ecad116bcdf9125dd9c75633241de2e7295
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2023-06-19 14:42:42 +0000

    ClownUI: Redesign Settings Style
    Big thanks for ideas
    
    [Settings]
    - using settings base layout from Xdroid
    
    [Dashboard]
    - Add pref card from PalyrimOS
    - Add Usercard Layout From Project Blaze
    
    [Firmware Info]
    - Add ClownUI Banner with Wallpaper blur view from TequilaOS
    - Redesign New ClownUI Logo Banner
    
    [About Phone]
    - Redesign About Phone From Project Blaze
    
    Co-authored-by: beingmishra <rahul731999@gmail.com>
    Co-authored-by: dlwlrma123 <alexfinhart@gmail.com>
    Co-authored-by: anupam41 <anupamsamanta41@gmail.com>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: beingmishra <rahul731999@gmail.com>
    Co-authored-by: Maitreya29 <maitreyapatni30@gmail.com>
    Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: dlwlrma123 <alexfinhart@gmail.com>
    Co-authored-by: anupam41 <anupamsamanta41@gmail.com>
    Co-authored-by: jijinraj <jijinraj401@gmail.com>
    Co-authored-by: 00day0 <therandomuser11@gmail.com>
    Co-authored-by: Bartłomiej Rudecki <ejbtrd@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1248,7 +1248,7 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     @ColorInt
     public static int getHomepageIconColor(Context context) {
-        return getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
+        return getColorAttrDefaultColor(context, android.R.attr.colorAccent);
     }
 
     /**
@@ -1256,7 +1256,7 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     @ColorInt
     public static int getHomepageIconColorHighlight(Context context) {
-        return context.getColor(R.color.accent_select_primary_text);
+        return getColorAttrDefaultColor(context, android.R.attr.colorAccent);
     }
 
     /**
