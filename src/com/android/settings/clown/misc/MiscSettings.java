dc17ee17f056052d722072344428cc4ffa780bff
Peter Meiser <peter.meiser@gmx.com>
2024-08-17 22:50:08 +0800

    [SQUASH] Add Option menu for spoofing taken from derpspace
    DerpSpace: General tweaks: User toggle for unlimited g00gle ph0t0s st0rage [2/2]
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I5fca5843a2178f5a39a753ff77c6194220a3026d
    
    DerpSpace: Allow toggling netflix spoofing [2/2]
    
    Reland "DerpSpace: General tweaks: User toggle for unlimited g00gle ph0t0s st0rage [2/2]"
    This reverts commit 369e658.
    
    Co-authored-by: Matthias  <spezi77@users.noreply.github.com>
    Co-authored-by: ShevT  <ShevT@users.noreply.github.com>
    Co-authored-by: NurKeinNeid <mralexman3000@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ src/com/android/settings/clown/misc/MiscSettings.java
@@ -0,0 +1,119 @@
+package com.android.settings.clown.misc;
+
+import android.app.ActivityManagerNative;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.IWindowManager;
+import android.view.View;
+import android.view.WindowManagerGlobal;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreferenceCompat;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+
+import com.android.settingslib.development.SystemPropPoker;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GeneralTweaks extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
+    private static final String SYS_INTEGRITY_SPOOF = "persist.sys.pixelprops.integrity";
+
+    private SwitchPreferenceCompat mPhotosSpoof;
+    private SwitchPreferenceCompat mNetFlixSpoof;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.misc_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPhotosSpoof = (SwitchPreferenceCompat) prefSet.findPreference(KEY_PHOTOS_SPOOF);
+        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
+        mPhotosSpoof.setOnPreferenceChangeListener(this);
+
+        mNetFlixSpoof = (SwitchPreferenceCompat) findPreference(KEY_NETFLIX_SPOOF);
+        mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
+        mNetFlixSpoof.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CLOWN;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	Context mContext = getActivity().getApplicationContext();
+	ContentResolver resolver = mContext.getContentResolver();
+        if (preference == mPhotosSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
+            return true;
+        } else if (preference == mNetFlixSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_NETFLIX_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
+            return true;
+        }
+        return false;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider() {
+            @Override
+            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                    boolean enabled) {
+                final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                final SearchIndexableResource sir = new SearchIndexableResource(context);
+                sir.xmlResId = R.xml.general_tweaks;
+                result.add(sir);
+                return result;
+            }
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                final List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
+    };
+}
